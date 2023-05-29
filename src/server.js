10a9c2ba57803712f90733fe8e0c25c0908974b0
mteusbarbosa <mateus.bm13@gmail.com>
2023-05-29 16:48:55 -0300

    database

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
 import http from 'node:http'
 import { json } from './middleware/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database() 
 
 const server = http.createServer(async (req, res) => {
-
   const { method, url } = req
 
   await json(req, res)
 
   if(method === 'GET' && url === '/users'){
-    return res.end(JSON.stringify(users))
+    const users = database.select('users')
 
+    return res.end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users'){
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
-      name: name,
-      email: email, 
-    })
+      name,
+      email, 
+    }
+
+    database.insert('users', user)
     return res.writeHead(201).end()
   }
 
